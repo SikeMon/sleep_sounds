002c39605a795d43628a4ac82009bdb6c4db87e2
yewintun <yetun1242@gmail.com>
2024-08-15 18:16:18 +0630

    4

--- android/app/src/main/java/com/sleepsounds/relax/MainActivity.kt
+++ android/app/src/main/java/com/sleepsounds/relax/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sleepsounds.relaxtion
+package com.sleepsounds.relax
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/sleepsounds/relax/MainApplication.kt
+++ android/app/src/main/java/com/sleepsounds/relax/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.sleepsounds.relaxtion
+package com.sleepsounds.relax
 
 import android.app.Application
 import android.content.res.Configuration
